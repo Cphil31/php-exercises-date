d7e88a2586097cc2539b24dcfa1089f1c9995a87
Cphil31 <cecile_philippe@live.fr>
2017-01-13 12:27:20 +0100

    step 3 fait avec le bonus en francais

--- index1.php
+++ index1.php
@@ -1,5 +1,35 @@
-<?php 
-date_default_timezone_set('UTC');
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+</head>
+<body>
+	<?php 
+	setlocale (LC_TIME, 'fr_FR','fra');
 
-echo date("j/m/Y");
- ?>
+	date_default_timezone_set('Europe/Paris');
+	// --- La setlocale() fonctionnne pour strftime mais pas pour DateTime->format()
+	setlocale(LC_TIME, 'fr_FR.utf8','fra');// OK
+	// strftime("jourEnLettres jour moisEnLettres annee") de la date courante
+	?>
+	<div> 
+		<?php 
+		echo 'exercice 1: '.date("j/m/Y");
+		?>
+	</div>
+
+	<div>
+		<?php 
+		echo 'exercice 2: '.date("d-m-y");
+		?> 
+	</div>
+
+	<div>
+		<?php 
+		echo 'exercice 3: '.strftime("%A %B %G");
+		?> 
+	</div>
+</body>
+</html>
+
